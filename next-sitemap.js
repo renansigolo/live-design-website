c86df9dc28b60ea68967ddb3da2bd51c6df9398d
Renan Sigolo <renan.sigolo@gmail.com>
2021-10-20 04:34:43 +1100

    chore: update renovate config

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,4 +1,4 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://livedesign.com.br",
-  generateRobotsTxt: true
-};
+  siteUrl: process.env.SITE_URL || 'https://livedesign.com.br',
+  generateRobotsTxt: true,
+}
