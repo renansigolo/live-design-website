3527a47cb1249d5bce216ccde3ad7cdd9509ee37
Renan Sigolo <renan.sigolo@gmail.com>
2022-04-06 22:45:22 +1000

    feat: add images

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -5,6 +5,24 @@ import Modal from './Modal'
 
 const modalData = [
   {
+    id: 'gois-street',
+    title: 'Gois Street',
+    url: 'https://www.behance.net/gallery/134595173/Gois-Street-Identidade-Visual',
+    images: [1, 2],
+  },
+  {
+    id: 'beloni-acessorios',
+    title: 'Beloni Acessórios',
+    url: 'https://www.behance.net/gallery/128442559/Beloni-Acessorios-Identidade-Visual',
+    images: [1, 2],
+  },
+  {
+    id: 'ocl-engenheiro-civil',
+    title: 'OCL Engenheiro Civil',
+    url: 'https://www.behance.net/gallery/125338315/Otavio-Campos-Lopes-Eng-Civil-Identidade-Visual',
+    images: [1, 2],
+  },
+  {
     id: 'my-adhd',
     title: 'My ADHD',
     url: 'https://www.behance.net/gallery/120762549/MyADHD-Identidade-Visual-e-UI-UX',
@@ -16,24 +34,24 @@ const modalData = [
     url: 'https://www.behance.net/gallery/90808651/Robinpack-Identidade-Visual',
     images: [0, 1, 2],
   },
-  {
-    id: 'gata-mineira',
-    title: 'Gata Mineira',
-    url: 'https://www.behance.net/gallery/106178891/Gata-Mineira-Conceito-de-Identidade-Visual',
-    images: [0, 1, 2],
-  },
-  {
-    id: 'inove',
-    title: 'Inove 4.0',
-    url: 'https://www.behance.net/gallery/97911067/Inove-40-Consultoria-de-Negocios-Identidade-Visual',
-    images: [0, 1],
-  },
-  {
-    id: 'pacocao',
-    title: 'Paçocão',
-    url: 'https://www.behance.net/gallery/111853811/Pacocao-Identidade-Visual',
-    images: [0, 1],
-  },
+  // {
+  //   id: 'gata-mineira',
+  //   title: 'Gata Mineira',
+  //   url: 'https://www.behance.net/gallery/106178891/Gata-Mineira-Conceito-de-Identidade-Visual',
+  //   images: [0, 1, 2],
+  // },
+  // {
+  //   id: 'inove',
+  //   title: 'Inove 4.0',
+  //   url: 'https://www.behance.net/gallery/97911067/Inove-40-Consultoria-de-Negocios-Identidade-Visual',
+  //   images: [0, 1],
+  // },
+  // {
+  //   id: 'pacocao',
+  //   title: 'Paçocão',
+  //   url: 'https://www.behance.net/gallery/111853811/Pacocao-Identidade-Visual',
+  //   images: [0, 1],
+  // },
   {
     id: 'olivier',
     title: 'Olivier Semijoias',
