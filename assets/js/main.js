749c50f382dd148ab9fce64d4d2aaf1472aefd76
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-29 23:39:57 +1000

    fix: cleanup scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 // import M from '@materializecss/materialize'
 // import * as materialLibrary from './node_modules/@materializecss/materialize/dist/js/materialize.min.js'
 import './modules/gtag'
-import './modules/home'
 import './modules/navigation'
 import './modules/orcamento'
 import './modules/portfolio'
