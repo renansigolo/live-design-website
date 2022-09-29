377cb78ae8acced58fc79adc77ee4edc980f464e
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-30 00:34:44 +1000

    fix: analytics script

--- assets/js/modules/gtag.js
+++ assets/js/modules/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = 'UA-104410576-1'
+export const GA_TRACKING_ID = 'G-RWJZJJGP3H'
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
@@ -17,17 +17,17 @@ export const event = ({ action, category, label, value }) => {
 }
 
 // Record the gtag event when someone clicks on the chosen link or button.
-export const gtagFormConversion = () => {
-  const url = 'https://form.jotformz.com/92974452958677'
-  const callback = function () {
-    if (typeof url !== 'undefined') {
-      window.open(url, '_blank')
-    }
-  }
+// export const gtagFormConversion = () => {
+//   const url = 'https://form.jotformz.com/92974452958677'
+//   const callback = function () {
+//     if (typeof url !== undefined) {
+//       window.open(url, '_blank')
+//     }
+//   }
 
-  gtag('event', 'conversion', {
-    send_to: 'AW-840306545/jF4gCLidy7UBEPGe2JAD',
-    event_callback: callback,
-  })
-  return false
-}
+//   gtag('event', 'conversion', {
+//     send_to: 'AW-840306545/jF4gCLidy7UBEPGe2JAD',
+//     event_callback: callback,
+//   })
+//   return false
+// }
--- assets/js/modules/orcamento.js
+++ assets/js/modules/orcamento.js
@@ -1,6 +1,18 @@
+// Record the gtag event when someone clicks on the chosen link or button.
 const orcamentoElements = document.querySelectorAll('.tagOrcamento')
 orcamentoElements.forEach((element) => {
-  element.addEventListener('click', (evt) =>
-    console.log(`${JSON.stringify(evt)} clicked`)
-  )
+  element.addEventListener('click', () => {
+    const url = 'https://form.jotformz.com/92974452958677'
+    const callback = function () {
+      if (typeof url !== undefined) {
+        window.open(url, '_blank')
+      }
+    }
+
+    window.gtag('event', 'conversion', {
+      send_to: 'AW-840306545/jF4gCLidy7UBEPGe2JAD',
+      event_callback: callback,
+    })
+    return false
+  })
 })
