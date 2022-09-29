121ca2f214738570452910c3f1023b6fdc5433c4
Renan Sigolo <renan.sigolo@gmail.com>
2022-09-30 00:23:07 +1000

    fix: navigation menu

--- assets/js/modules/navigation.js
+++ assets/js/modules/navigation.js
@@ -1,17 +1,29 @@
 // Toogle Mobile Menu Background
-let activeSidebar = false
-const toggleOpen = () => {
-  !activeSidebar
-    ? (document.body.style.overflow = 'hidden')
-    : (document.body.style.overflow = '')
+let activeSidebar = true
+
+const mobileMenu = document.querySelector('#mobile-menu')
+const mobileMenuBackground = document.querySelector('#mobile-menu-background')
+const mobileMenuItems = document.querySelectorAll('#mobile-menu li')
+
+const toggleSidebar = () => {
+  if (activeSidebar) {
+    document.body.style.overflow = 'hidden'
+    mobileMenu.classList.add('nav-active')
+    mobileMenuBackground.classList.add('overflow-active')
+  } else {
+    document.body.style.overflow = ''
+    mobileMenu.classList.remove('nav-active')
+    mobileMenuBackground.classList.remove('overflow-active')
+  }
   activeSidebar = !activeSidebar
 }
+mobileMenuBackground.addEventListener('click', toggleSidebar)
+mobileMenuItems.forEach((item) => {
+  item.addEventListener('click', toggleSidebar)
+})
 document
   .querySelector('[data-target="mobile-menu"]')
-  .addEventListener('click', () => {
-    console.log('mobile-menu clicked', activeSidebar)
-    toggleOpen()
-  })
+  .addEventListener('click', () => toggleSidebar())
 
 /** Show or Hide the button when scroll after portfolio */
 const btnFixed = document.querySelector('#btn-fixed')
