d28a0fcc10ebaafaad2e7bf77aac0ed55074dd32
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-04 12:42:21 +1100

    fix: open modal logic

--- assets/js/modules/modal.js
+++ assets/js/modules/modal.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const modalTriggers = document.querySelectorAll('.modal-trigger')
-  const container = document.querySelector('.modal-container')
+let modalId
+let modalElement
 
+const modalTriggers = document.querySelectorAll('.modal-trigger')
+const modalContainer = document.querySelector('.modal-container')
+
+document.addEventListener('DOMContentLoaded', () => {
   modalTriggers.forEach((el) => {
     el.addEventListener('click', (ref) => {
-      const modalId = ref.target.dataset.modal
-      document.querySelector(`#${modalId}`).style.display = 'flex'
-      container.style.display = 'flex'
+      modalId = ref.target.dataset.modal
+      modalElement = document.querySelector(`#${modalId}`)
+
+      modalElement.style.display = 'flex'
+      modalContainer.style.display = 'flex'
       document.body.style.overflow = 'hidden'
     })
   })
 
-  container.addEventListener('click', (ref) => {
-    if (ref.target === container) {
-      container.style.display = 'none'
+  modalContainer.addEventListener('click', (ref) => {
+    if (ref.target === modalContainer) {
+      modalContainer.style.display = 'none'
       document.body.style.overflow = ''
     }
+    modalElement.style.display = 'none'
   })
 })
