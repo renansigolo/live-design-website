986d864eefbe53e7056e8f0c9ce09b242fb66cdc
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-06 12:17:31 +1100

    chore: update project config

--- scripts/setup-hooks.sh
+++ scripts/setup-hooks.sh
@@ -9,16 +9,16 @@ echo '#!/bin/sh
 linter_exit_code=1
 all_files=$(git diff --staged --name-only --diff-filter=d)
 js_files=$(git diff --staged --name-only --diff-filter=d | grep .js$)
-scss_files=$(git diff --staged --name-only --diff-filter=d | grep .scss$)
+style_files=$(git diff --staged --name-only --diff-filter=d | grep ".scss\|.css$")
 
 ./node_modules/.bin/prettier --ignore-unknown --write $all_files &&
     ./node_modules/.bin/eslint $js_files --quiet --fix &&
-    ./node_modules/.bin/stylelint $scss_files --stdin --quiet --fix &&
+    ./node_modules/.bin/stylelint $style_files --stdin --quiet --fix &&
     ./node_modules/.bin/prettier --check $all_files
 
 linter_exit_code=$?
 
-git add -f $js_files $scss_files
+git add -f $js_files $style_files
 
 if [ $linter_exit_code -ne 0 ]; then
     echo "❌ Linter errors have occurred"
