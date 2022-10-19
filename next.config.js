554cc234907574dfab95074d67b388a5ea8249c2
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-19 13:54:47 +1100

    ci: update headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,31 @@
+const securityHeaders = [
+  {
+    key: 'Referrer-Policy',
+    value: 'no-referrer-when-downgrade',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+]
+
+/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
   swcMinify: true,
@@ -9,5 +37,14 @@ module.exports = {
   i18n: {
     locales: ['pt-BR'],
     defaultLocale: 'pt-BR',
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 }
