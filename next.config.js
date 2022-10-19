bd58332bdeee7df66a8e271a8f5c122ee5484311
Renan Sigolo <renan.sigolo@gmail.com>
2022-10-19 14:10:07 +1100

    ci: update headers

--- next.config.js
+++ next.config.js
@@ -8,20 +8,16 @@ const securityHeaders = [
     value: 'max-age=63072000; includeSubDomains; preload',
   },
   {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  {
-    key: 'X-XSS-Protection',
-    value: '1; mode=block',
-  },
-  {
     key: 'X-Content-Type-Options',
     value: 'nosniff',
   },
   {
     key: 'X-Frame-Options',
     value: 'SAMEORIGIN',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
   },
 ]
 
